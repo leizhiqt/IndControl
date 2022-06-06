966c13ec925a55c31000f14e7e74aa72a49fdd16
Leizhiqt <275421450@qq.com>
2022-06-06 10:10:29 +0800

    fixed v0.0002

--- src/protocol/modbustcp/ModBusTcp.cpp
+++ src/protocol/modbustcp/ModBusTcp.cpp
@@ -8,12 +8,90 @@
 #include "modbus.h"
 #include <thread>
 #include "ControlMain.h"
+#include "UWLog.h"
+
+modbus_t* ctx;
+modbus_mapping_t* mb_mapping;
+int server_socket = -1;
+
+std::thread thd_handle0;//主线程
+
+std::thread thd_handle_receive;//接收线程
+std::thread thd_handle_send;//发送线程
+
+std::list<uint8_t *> rbuf;
+uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
+
+int modbus_tcp_receive_thread_run()
+{
+    for(;;)
+    {
+        int rc;
+        rc = modbus_receive(ctx, query);
+        if(rc>1)
+        {
+
+            modbus_reply(ctx, query, rc, mb_mapping);
+
+            rbuf.push_back(query);//尾部添加
+
+            log_debug("modbus_receive len=%d",rc);
+
+//            controlMain->mWin->canClient->send(can_data);
+        }
+        else
+        {
+
+            printf("Connection Closed\n");
+
+            modbus_close(ctx);
+
+            modbus_tcp_accept(ctx, &server_socket);
+        }
+    }
+    printf("Quit the loop:%s\n", modbus_strerror(errno));
+}
+
+int modbus_tcp_send_thread_run()
+{
+    uint8_t can_data[13];
+    memset(can_data,'\0',sizeof(can_data));
+    can_data[0]=0x43;
+//    can_data[1]=0x06;
+//    can_data[2]=0x06;
+    can_data[3]=0x03;
+    can_data[4]=0xff;
+
+//    can_data[5]=0x11;
+//    can_data[6]=0x22;
+    can_data[7]=0x01;
+//    can_data[8]=0x44;
+//    can_data[9]=0x55;
+//    can_data[10]=0x66;
+//
+//    11-12
+
+    for(;;)
+    {
+
+        if(rbuf.size()==0){
+            Sleep(300);
+        }
+
+        uint8_t *modbus_rbuf = rbuf.front();
+        uint8_t b0 = *modbus_rbuf;
+
+        if(b0==0x18a|| b0==0x18b||b0==0x18c){
+            modbus_write_bits(ctx,1,13,can_data);
+        }
+        rbuf.pop_front();//头取出
+    }
+}
 
 int modbus_tcp_start(char* ipAddr,int port)
 {
-    int server_socket = -1;
-	modbus_t* ctx;
-	modbus_mapping_t* mb_mapping;
+
+
 //	const char *ipAdd = "127.0.0.1";
     ctx = modbus_new_tcp(ipAddr, port);
 	modbus_set_debug(ctx, TRUE);
@@ -39,57 +117,26 @@ int modbus_tcp_start(char* ipAddr,int port)
 
 	modbus_tcp_accept(ctx, &server_socket);
 
-    uint8_t can_data[13];
-    memset(can_data,'\0',sizeof(can_data));
-    can_data[0]=0x43;
-//    can_data[1]=0x06;
-//    can_data[2]=0x06;
-    can_data[3]=0x03;
-    can_data[4]=0xff;
+    //启动成功
+    thd_handle_receive = std::thread(modbus_tcp_receive_thread_run);
+    thd_handle_send = std::thread(modbus_tcp_send_thread_run);
 
-//    can_data[5]=0x11;
-//    can_data[6]=0x22;
-    can_data[7]=0x01;
-//    can_data[8]=0x44;
-//    can_data[9]=0x55;
-//    can_data[10]=0x66;
-//
-//    11-12
-	for(;;)
-	{
-	    uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
-		int rc;
+    if (thd_handle_receive.joinable())
+        thd_handle_receive.join();
 
-        modbus_write_register(ctx,1,345);
+    if (thd_handle_send.joinable())
+        thd_handle_send.join();
 
-		rc = modbus_receive(ctx, query);
-		if(rc>1)
-		{
-		
-	        modbus_reply(ctx, query, rc, mb_mapping);
-            controlMain->mWin->canClient->send(can_data);
-		}
-		else
-		{
-		    
-			printf("Connection Closed\n");
-
-			modbus_close(ctx);
-
-			modbus_tcp_accept(ctx, &server_socket);
-		}
-	}
-
-	printf("Quit the loop:%s\n", modbus_strerror(errno));
-
-	modbus_mapping_free(mb_mapping);
+    modbus_mapping_free(mb_mapping);
 	modbus_close(ctx);
 	modbus_free(ctx);
 
 	return 0;
 }
 
-std::thread thd_handle0;
+
+
+
 
 int modbus_tcp_thread_start(char* ipAddr,int port)
 {
--- src/protocol/modbustcp/ModbusTcp.h
+++ src/protocol/modbustcp/ModbusTcp.h
@@ -1,6 +1,5 @@
 ﻿#ifndef MODBUSTCP_H
 #define MODBUSTCP_H
-#include "ModbusTcpClient.h"
 
 extern int modbus_tcp_thread_start(char* ipAddr,int port);
 extern void modbus_tcp_thread_wait();
