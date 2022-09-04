d3d6c5d607bc2b26245433034388da40b2d1c2cb
Leizhiqt <275421450@qq.com>
2022-09-04 17:15:01 +0800

    v0.0047

--- uimain/Conf.cpp
+++ uimain/Conf.cpp
@@ -14,58 +14,58 @@ Conf::Conf()
     log_open("IndControl.txt");
 
     //读取配置信息
-    QSettings read(qApp->applicationDirPath() + QString("/config.ini"),QSettings::IniFormat);
+    QSettings confSeting(qApp->applicationDirPath() + QString("/config.ini"),QSettings::IniFormat);
 
     //位姿服务IP
-    xueLangIp = read.value("/XueLang/XueLangIp").toString();
+    xueLangIp = confSeting.value("/XueLang/XueLangIp").toString();
     //位姿服务端口
-    xueLangPort = read.value("/XueLang/XueLangPort").toString().toInt();
+    xueLangPort = confSeting.value("/XueLang/XueLangPort").toString().toInt();
 
     //数据库IP地址
-    pgHostIp = read.value("/Database/PgHostIp").toString();
+    pgHostIp = confSeting.value("/Database/PgHostIp").toString();
     //数据库端口
-    pgHostPort = read.value("/Database/PgHostPort").toString();
+    pgHostPort = confSeting.value("/Database/PgHostPort").toString();
     //用户名
-    pgUser = read.value("/Database/PgUser").toString();
+    pgUser = confSeting.value("/Database/PgUser").toString();
     //用户密码
-    pgPass = read.value("/Database/PgPass").toString();
+    pgPass = confSeting.value("/Database/PgPass").toString();
     //数据库名称
-    pgDataBase = read.value("/Database/PgDatabase").toString();
+    pgDataBase = confSeting.value("/Database/PgDatabase").toString();
 
     //ModBus服务
-    modBusPort = read.value("/Modbus/ModbusPort").toString().toInt();
+    modBusPort = confSeting.value("/Modbus/ModbusPort").toString().toInt();
 
     //总线服务IP
-    canOpenIp = read.value("/CanOpen/CanOpenip").toString();
+    canOpenIp = confSeting.value("/CanOpen/CanOpenip").toString();
     //总线端口
-    canOpenPort = read.value("/CanOpen/CanOpenPort").toString().toInt();
+    canOpenPort = confSeting.value("/CanOpen/CanOpenPort").toString().toInt();
 
     //WebSocket端口
-    websocketPort = read.value("/WebSocket/WebSocketPort").toString().toInt();
+    websocketPort = confSeting.value("/WebSocket/WebSocketPort").toString().toInt();
 
     //Modbus协同控制器IP
-    modbusSlaveIp = read.value("/ModbusSlave/SlaveIp").toString();
+    modbusSlaveIp = confSeting.value("/ModbusSlave/SlaveIp").toString();
     //Modbus协同控制器端口
-    modbusSlavePort = read.value("/ModbusSlave/SlavePort").toString().toInt();
+    modbusSlavePort = confSeting.value("/ModbusSlave/SlavePort").toString().toInt();
 
     //控制命令,这里加载到map直接取了就发
-    QSettings readControl(qApp->applicationDirPath() + QString("/ControlSetting.ini"), QSettings::IniFormat);
-    controlNameList = readControl.allKeys();
+    QSettings confSetingControl(qApp->applicationDirPath() + QString("/ControlSetting.ini"), QSettings::IniFormat);
+    controlNameList = confSetingControl.allKeys();
     char binary[128];
     for(int i = 0; i < controlNameList.size(); i++){
-          QString key=controlNameList.at(i);
-          key.remove(QRegExp("\\s"));
-          std::string stdkey= key.toStdString();
-          QByteArray value =readControl.value(key).toByteArray();
-          QString qstr = QString(value);
-          qstr.remove(QRegExp("\\s"));
-          const char *p = qstr.toLatin1().constData();
-          memset(binary,'\0',sizeof(binary));
-          hexs_to_binary(p,qstr.length(),(unsigned char *)binary);
-          QByteArray canFrame(binary,qstr.length()/2);
-          conf_can_packs.insert(std::pair<std::string,QByteArray>(stdkey,canFrame));
-          //printf_hex((unsigned char*)binary,canFrame.length());
-          //log_debug("key:%s",stdkey.c_str());
+        QString key=controlNameList.at(i);
+        key.remove(QRegExp("\\s"));
+        std::string stdkey= key.toStdString();
+        QByteArray value =confSetingControl.value(key).toByteArray();
+        QString qstr = QString(value);
+        qstr.remove(QRegExp("\\s"));
+        const char *p = qstr.toLatin1().constData();
+        memset(binary,'\0',sizeof(binary));
+        hexs_to_binary(p,qstr.length(),(unsigned char *)binary);
+        QByteArray canFrame(binary,qstr.length()/2);
+        conf_can_packs.insert(std::pair<std::string,QByteArray>(stdkey,canFrame));
+        //printf_hex((unsigned char*)binary,canFrame.length());
+        //log_debug("key:%s",stdkey.c_str());
     }
 }
 
